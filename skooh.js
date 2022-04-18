5a842dfa5fc88b048856694e239834c2d5f56b65
Austin Gatlin <austin@gatlin.io>
2022-04-18 18:01:46 +1000

    Add loop to check for all valid hooks.

--- skooh.js
+++ skooh.js
@@ -5,20 +5,21 @@ const fs = require("fs");
 const args = process.argv.slice(2);
 
 if (args.length == 1 && args[0] == "prepare") {
-  prepare();
-}
-
-function prepare() {
   const hooks = getHooks("./package.json");
   writeHooks(hooks);
 }
 
 function writeHooks(hooks) {
-  const scriptTemplate = (hook) => `#!/bin/sh\n\n${hook}\n`;
-
-  const preCommit = hooks["pre-commit"];
-  if (preCommit)
-    fs.writeFileSync("./.git/hooks/pre-commit", scriptTemplate(preCommit));
+  for (const [hookName, hookString] of Object.entries(hooks)) {
+    if (validHooks.includes(hookName)) {
+      fs.writeFileSync(
+        `./.git/hooks/${hookName}`,
+        `#!/bin/sh\n\n${hookString}\n`
+      );
+    } else {
+      console.error(`skooh: invalid hook name: ${hookName}`);
+    }
+  }
 }
 
 function getHooks(packageJson) {
@@ -26,3 +27,34 @@ function getHooks(packageJson) {
   const json = JSON.parse(jsonFile);
   return json.hooks;
 }
+
+const validHooks = [
+  "applypatch-msg",
+  "pre-applypatch",
+  "post-applypatch",
+  "pre-commit",
+  "pre-merge-commit",
+  "prepare-commit-msg",
+  "commit-msg",
+  "post-commit",
+  "pre-rebase",
+  "post-checkout",
+  "post-merge",
+  "pre-push",
+  "pre-receive",
+  "update",
+  "proc-receive",
+  "post-receive",
+  "post-update",
+  "reference-transaction",
+  "push-to-checkout",
+  "pre-auto-gc",
+  "post-rewrite",
+  "sendemail-validate",
+  "fsmonitor-watchman",
+  "p4-changelist",
+  "p4-prepare-changelist",
+  "p4-post-changelist",
+  "p4-pre-submit",
+  "post-index-change",
+];
